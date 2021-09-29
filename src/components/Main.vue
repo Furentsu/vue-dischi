44f3284f0259726835cbe56187cd5a974bb6ef62
Furentsu <eugeniofranzoso@gmail.com>
2021-09-29 16:25:35 +0200

    Initialization

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <Albums />
+  </div>
+</template>
+
+<script>
+import Albums from '../components/Albums.vue'
+
+export default {
+    name: 'Main',
+    components: {
+        Albums,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+
+</style>
